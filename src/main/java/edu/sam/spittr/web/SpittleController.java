3f0b03c8db5a203d7069c38b98b782f7fef60fc8
mup <man.upon.problems@gmail.com>
2019-06-09 21:45:47 +0300

    Small change in SpittleController

--- src/main/java/edu/sam/spittr/web/SpittleController.java
+++ src/main/java/edu/sam/spittr/web/SpittleController.java
@@ -1,16 +1,22 @@
 package edu.sam.spittr.web;
 
 
+import edu.sam.spittr.Spittle;
 import edu.sam.spittr.data.SpittleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/spittles")
 public class SpittleController {
+    private static final String MAX_LONG_AS_STRING = Long.toString(Long.MAX_VALUE);
+
     private SpittleRepository spittleRepository;
 
     @Autowired
@@ -19,8 +25,17 @@ public class SpittleController {
     }
 
     @RequestMapping(method=RequestMethod.GET)
-    public String spittles(Model model) {
+    public String spittles(Model model,
+           @RequestParam(value="max", defaultValue=MAX_LONG_AS_STRING) long max,
+           @RequestParam(value="count", defaultValue="20") int count) {
         model.addAttribute(spittleRepository.findSpittles(Long.MAX_VALUE, 20));
         return "spittles";
     }
 }
+
+    @RequestMapping(method=RequestMethod.GET)
+    public  spittles(
+
+            {
+        return spittleRepository.findSpittles(max, count);
+    }
