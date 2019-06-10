c0c8bff206ff61acbe1c4b0e21aaaf2a28518841
Sinitsa Dzmitriy <D.Sinitsa@sam-solutions.com>
2019-06-10 13:05:48 +0300

    Various <spring: url> usage

--- src/main/java/edu/sam/spittr/web/SpittleController.java
+++ src/main/java/edu/sam/spittr/web/SpittleController.java
@@ -1,12 +1,15 @@
 package edu.sam.spittr.web;
 
 
+import edu.sam.spittr.Spittle;
 import edu.sam.spittr.data.SpittleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/spittles")
@@ -20,7 +23,13 @@ public class SpittleController {
 
     @RequestMapping(method=RequestMethod.GET)
     public String spittles(Model model) {
-        model.addAttribute(spittleRepository.findSpittles(Long.MAX_VALUE, 20));
-        return "spittles";
+        // log.debug("spittles started with model={} and params={}", model, param);
+        List<Spittle> spittles = spittleRepository.findSpittles(Long.MAX_VALUE, 20);
+        // log.debug("Found following splittes:{}", spittles);
+        model.addAttribute(spittles);
+
+        String resultURL = "spittles";
+        // log.debug("spittles finished, result URL={} and updated model={}", resultURL, model);
+        return resultURL;
     }
 }
