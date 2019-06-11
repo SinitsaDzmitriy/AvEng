3c08627ecde72501e55bdec03ec3fd0be36abcc4
mup <man.upon.problems@gmail.com>
2019-06-11 18:10:17 +0300

    Intermediate commit

--- src/main/java/edu/sam/spittr/web/SpittleController.java
+++ src/main/java/edu/sam/spittr/web/SpittleController.java
@@ -1,10 +1,11 @@
 package edu.sam.spittr.web;
 
-
+import edu.sam.spittr.Spittle;
 import edu.sam.spittr.data.SpittleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -27,13 +28,27 @@ public class SpittleController {
     @RequestMapping(value="/create", method=RequestMethod.GET)
     public String create(Model model) {
         // Add a Spittle object with "spittle" key in the model
-        // It uses in
+        // It's referenced at createSpittleForm JSP
         model.addAttribute(new Spittle());
         return "createSpittleForm";
     }
 
     @RequestMapping(value="/create", method=RequestMethod.POST)
     public String persist(Spittle spittleToPersist) {
+        spittleRepository.save(spittleToPersist);
+        return "allSpittles";
+    }
+
+    @RequestMapping(value="/edit/{spittleId}", method=RequestMethod.GET)
+    public String create(@PathVariable long spittleId, Model model) {
+        // Add a Spittle object with specific ID and "spittle" key in the model
+        // It's referenced in the editSpittleForm JSP view
+        model.addAttribute(spittleRepository.findById(spittleId));
+        return "editSpittleForm";
+    }
+
+    @RequestMapping(value="/change", method=RequestMethod.POST)
+    public String perist(Spittle spittleToPersist) {
         spittleRepository.save(spittleToPersist);
         return "allSpittles";
     }
