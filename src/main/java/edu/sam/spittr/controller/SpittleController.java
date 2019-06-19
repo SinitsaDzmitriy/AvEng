bb1a203d7a86862d43c7fb54817a6f56b16eb614
Sinitsa Dzmitriy <D.Sinitsa@sam-solutions.com>
2019-06-19 19:42:06 +0300

    @ModeAttribute

--- src/main/java/edu/sam/spittr/controller/SpittleController.java
+++ src/main/java/edu/sam/spittr/controller/SpittleController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -60,7 +61,7 @@ public class SpittleController{
     // that should immediately follow the @Valid-annotated parameter
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public String create(@Valid SpittleDTO spittleToPersist, Errors errors) {
+    public String create(@Valid @ModelAttribute("spittleDTO") SpittleDTO spittleToPersist, Errors errors) {
         if (errors.hasErrors()) {
             return "spittleCreationForm";
         }
