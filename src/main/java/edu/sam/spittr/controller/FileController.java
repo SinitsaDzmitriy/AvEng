adf39cfbe796f94b831b748ca58386ae8f769923
Sinitsa Dzmitriy <D.Sinitsa@sam-solutions.com>
2019-07-23 13:42:24 +0300

    File upload validation is skipped for now

--- src/main/java/edu/sam/spittr/controller/FileController.java
+++ src/main/java/edu/sam/spittr/controller/FileController.java
@@ -4,7 +4,6 @@ import edu.sam.spittr.validator.FileValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
 
